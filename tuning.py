388e9c0e86b045801da84e5f48e1fad1e0b7a24c
IngvarBjarki <ingvarbjarki@gmail.com>
2018-06-04 18:12:32 +0200

    extended the search for parameteres

--- tuning.py
+++ tuning.py
@@ -48,12 +48,12 @@ def sgd(all_input_params):
     X, y = shuffle(X, y)
     
     num_dimensions = len(X[0])
-    num_in_batch = [1, 2, 5, 10, 50, 75, 100, 150, 200, 250]
+    num_in_batch = [1, 2, 5, 10, 50, 75, 100, 150, 200, 250, 300, 350, 400]
     epochs = 1
     k_splits = 5
     learning_rates = [1/np.sqrt(t + 1) for i in range(epochs) for t in range(amount_in_interval[-1])]
     epsilons = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 10, float('Inf')] # inf makes the noise go to zero -- equal to having no noise
-    weight_decays = [ 10**(-1), 10**(-2), 10**(-5), 10**(-8), 10**(-11)]
+    weight_decays = [ 1, 0.5, 10**(-1), 10**(-2), 10**(-5), 10**(-8), 10**(-11), 10**(-15)]
     
     parameters = {'batch_size':[], 'weight_decay':[], 'error_rate':[]}
     optimal_results = {}
